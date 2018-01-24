6c98c7f7777d350610dcfdcfbc829b04f4592b6c
AniruddhoMajumder <aniruddho1997@gmail.com>
2018-01-25 03:56:04 +0530

    Create DefaultValues.java

---
+++ WEB-INF/classes/defaults/DefaultValues.java
@@ -0,0 +1,7 @@
+package defaults;
+
+public class DefaultValues{
+	public static final String DBURL = "jdbc:mysql://localhost:3306/stockmarket";
+	public static final String DBUSER = "root";
+	public static final String DBPASS = "root";
+}
